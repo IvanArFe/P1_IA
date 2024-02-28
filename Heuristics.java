75495beb0ff70f72f4ed72ddab2002991e51b663
Ivan Arenal <ivan.arenal@estudiants.urv.cat>
2024-02-28 15:33:01 +0100

    Initial commit

---
+++ Heuristics.java
@@ -0,0 +1,22 @@
+public class Heuristics {
+    public static float Heuristic1(State currentState, State targetState, float[][] map){
+        /* TODO: Implement a heuristic 
+         * You CANNOT change the input parameters and return type.
+         * The value returned can ONLY be based on the current state and the target state, NOT intermediate states.
+         */
+    }
+
+    public static float Heuristic2(State currentState, State targetState, float[][] map){
+        /* TODO: Implement a heuristic 
+         * You CANNOT change the input parameters and return type.
+         * The value returned can ONLY be based on the current state and the target state, NOT intermediate states.
+         */
+    }
+
+    public static float Heuristic3(State currentState, State targetState, float[][] map){
+        /* TODO: Implement a heuristic 
+         * You CANNOT change the input parameters and return type.
+         * The value returned can ONLY be based on the current state and the target state, NOT intermediate states.
+         */
+    }
+}
---
+++ Map.java
@@ -0,0 +1,58 @@
+public class Map {
+    private char[][] charMap;
+    private float[][] costMap;
+
+    public Map(char[][] charMap){
+        this.charMap = charMap;
+        costMap = generateCostMap();
+    }
+    
+    public char[][] getCharMap(){
+        return charMap;
+    }
+
+    public float[][] getCostMap(){
+        return costMap;
+    }
+
+    private float[][] generateCostMap(){
+        float[][] costMap = new float[charMap.length][charMap[0].length];        
+        // Cost que te anar a cada cel·la, hem d'omplir els TO DO del pas per cada cel·la.
+        float cost = -1;
+        for(int row=0; row < charMap.length; row++){
+            for(int col=0; col < charMap[0].length; col++){
+                switch(charMap[row][col]){
+                    case 'M': cost= -1;// TODO: Define cost corresponding to "Mountain" ("Muntanya")
+                    break;
+                    case 'N': cost= 5;// TODO: Define cost corresponding to "Empty" ("Buit")
+                    break;
+                    case 'A': cost= 4;// TODO: Define cost corresponding to "Village" ("Aldea")
+                    break;
+                    case 'P': cost= 2;// TODO: Define cost corresponding to "Town" ("Poble")
+                    break;
+                    case 'C': cost= 0;// TODO: Define cost corresponding to "City" ("Ciutat")
+                    break;
+                }
+                costMap[row][col] = cost;
+            }
+        }
+
+        return costMap;
+    }
+
+    public String toString(){
+        String text = "";
+
+        for (char[] row : charMap) {
+            for (char cell : row){
+                text+=cell+" ";
+            }
+            text+="\n";
+        }
+
+        // Remove last enter
+        text = text.substring(0, text.length()-1);
+
+        return text;
+    }
+}
